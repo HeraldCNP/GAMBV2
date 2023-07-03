9a8500ab7332f4188db46a6abb6507c18b94b0ab
Carlos Vedia Flores <49203954+carlosvediaflores@users.noreply.github.com>
2023-07-03 10:28:17 -0400

    probar jspdf-autotable

--- src/app/modules/hoja-ruta/pages/report/report.component.ts
+++ src/app/modules/hoja-ruta/pages/report/report.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ReportService } from '../../services/report.service';
+import {jsPDF} from 'jspdf';
+import autoTable from 'jspdf-autotable'
+
+
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.css'],
 })
 export class ReportComponent implements OnInit {
-  reportForm : any;
-  users:any;
-  destino:string = '';
-  estado:string = '';
-  del:string = '';
-  al:string = '';
+  reportForm: any;
+  users: any;
+  destino: string = '';
+  estado: string = '';
+  del: string = '';
+  al: string = '';
   seguimientos: any = [];
   fechaHoy = new Date().toISOString();
   fechaIni = new Date('01/01/2023').toISOString();
 
-  constructor(private fb: FormBuilder, private reportService:ReportService,) {
+  constructor(private fb: FormBuilder, private reportService: ReportService) {
     this.reportForm = this.fb.group({
       funcionario: ['', Validators.required],
       estado: [''],
@@ -28,26 +32,23 @@ export class ReportComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.reportService.getAllUsers().subscribe(
-      data => {
-        this.users = data;
-        console.log(this.users);
-      }
-    );
+    this.reportService.getAllUsers().subscribe((data) => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
-
 
   get form() {
     return this.reportForm.controls;
   }
 
-  obtenerHojasRutas(form:any){
+  obtenerHojasRutas(form: any) {
     console.log(form.value);
     this.destino = form.value.funcionario;
     this.estado = form.value.estado;
     this.del = form.value.del;
     this.al = form.value.al;
-    this.getSeguimientos()
+    this.getSeguimientos();
   }
 
   getSeguimientos() {
@@ -63,16 +64,18 @@ export class ReportComponent implements OnInit {
     //   this.datelt=this.year+1;
     // }
     this.reportService
-      .getAllSeguimientos(
-        this.destino,
-        this.estado,
-        this.del,
-        this.al,
-      )
+      .getAllSeguimientos(this.destino, this.estado, this.del, this.al)
       .subscribe((data) => {
         this.seguimientos = data.serverResponse;
         console.log(this.seguimientos);
-
       });
   }
+  imprimir() {
+    const doc = new jsPDF({orientation:"landscape", format:'letter'});
+    
+    autoTable(doc, { html: '#table', theme:'grid'})
+    //doc.autoTable({ html: 'htmlData'});
+    doc.output('dataurlnewwindow', { filename: 'comprobante.pdf'});
+  }
+  
 }
