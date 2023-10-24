540f2274e0b457e81e5af7f793e218714259c538
unknown <Heraldcnp@gmail.com>
2023-10-24 08:52:12 -0400

    fix salida

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   // api: "http://159.223.119.115:8000/api"
-  api: "http://192.168.1.123:8000/api"
+  api: "http://192.168.1.104:8000/api"
 };
 
 /*
